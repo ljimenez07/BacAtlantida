c6d3af6ea1269d8b2bf7f9fc2c3e6bc19cd8c477
cguillen <cguillen@testingsoft.com>
2016-11-08 23:37:19 -0600

    Ajustes del index, para que no se mezclen el chat y el promociones

--- frontEnd/www/plantillas/layout.js
+++ frontEnd/www/plantillas/layout.js
@@ -3,12 +3,12 @@
 templates['layout'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1, helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function";
 
-  return "<div class=\"panel panel-default\">\n  <div class=\"panel-heading\">\n    <div class=\"row header bck-red\">\n      <div class=\"col-md-4\">\n        <div class=\"header-menu-icon\"></div>\n      </div>\n      <div class=\"col-md-4\">\n        <img src=\"img/logo.png\">\n      </div>\n      <div class=\"col-md-4\">\n        <div class=\"header-chat-icon\"></div>\n      </div>\n    </div>\n    <div class=\"row text-center\">\n      <div class=\"col-md-12\">\n        "
+  return "<div class=\"panel panel-default\">\r\n	<div class=\"panel-heading\">\r\n		<div class=\"row bck-red\">\r\n			<div class=\"col-sm-4 col-md-4 col-lg-4	col-xl-4\">\r\n				<div class=\"header-menu-icon\" ></div>	\r\n			</div>\r\n			<div class=\"col-xs-4 col-sm-4 col-md-4 col-lg-4	col-xl-4\"> \r\n				<img src=\"img/logo.png\">\r\n			</div>\r\n			<div class=\"col-xs-4 col-sm-4 col-md-4 col-lg-4	col-xl-4\">\r\n				<div class=\"header-chat-icon\" />\r\n			</div>\r\n		</div>\r\n		<div class=\"row title\">\r\n			<div class=\"col-xs-12 col-sm-12 col-md-12 col-lg-12	col-xl-12\">\r\n				"
     + container.escapeExpression(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
-    + "\n      </div>\n    </div>\n  </div>\n  <div class=\"panel-body\">\n    "
+    + "\r\n			</div>\r\n		</div>\r\n	</div>\r\n	<div class=\"panel-body\">\r\n		"
     + ((stack1 = ((helper = (helper = helpers.body || (depth0 != null ? depth0.body : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"body","hash":{},"data":data}) : helper))) != null ? stack1 : "")
-    + "\n  </div>\n</div>\n"
+    + "\r\n	</div>\r\n</div>\r\n"
     + ((stack1 = ((helper = (helper = helpers.DebajoDelPanel || (depth0 != null ? depth0.DebajoDelPanel : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"DebajoDelPanel","hash":{},"data":data}) : helper))) != null ? stack1 : "")
-    + "\n";
+    + "\r\n<script>\r\n	$(function(){\r\n		var sourceChat   = $(\"#chatInput\").html();\r\n		var templateChat = Handlebars.compile(sourceChat);\r\n		var contextChat = {};\r\n		var htmlChat    = templateChat(contextChat);\r\n\r\n		var context = {title: \"Iniio\", body: ofertas(), DebajoDelPanel:\"\"};\r\n		var html    = Handlebars.templates.layout(context);\r\n\r\n		$(\".app\").html(html);\r\n	});\r\n</script>";
 },"useData":true});
 })();
