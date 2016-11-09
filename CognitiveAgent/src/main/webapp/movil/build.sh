9189cf33d8962b2cf287d948b60b9cc1123709bd
cguillen <cguillen@testingsoft.com>
2016-11-09 16:55:22 -0600

    Fix para que se setee la url del server a la parte movil

--- CognitiveAgent/src/main/webapp/movil/build.sh
+++ CognitiveAgent/src/main/webapp/movil/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo var serverDomain = "$1"; > ./www/js/globals.js
+echo "var serverDomain = \"$1\";" > ./www/js/globals.js
 handlebars ./www/plantillas/chats.handlebars -f ./www/js/chats.handlebars.js 
 handlebars ./www/plantillas/layout.handlebars -f ./www/js/layout.handlebars.js 
 handlebars ./www/plantillas/ofertas.handlebars -f ./www/js/ofertas.handlebars.js 
