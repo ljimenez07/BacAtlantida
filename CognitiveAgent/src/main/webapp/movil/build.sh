3d6d37cfba2acb8ed392286bc39981a1c8d84350
cguillen <cguillen@testingsoft.com>
2016-11-09 15:35:05 -0600

    Script para actulizar los handlebars en QA

---
+++ CognitiveAgent/src/main/webapp/movil/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo var serverDomain = "$1"; > ./www/js/globals.js
+handlebars ./www/plantillas/chats.handlebars -f ./www/js/chats.handlebars.js 
+handlebars ./www/plantillas/layout.handlebars -f ./www/js/layout.handlebars.js 
+handlebars ./www/plantillas/ofertas.handlebars -f ./www/js/ofertas.handlebars.js 
+# cordova run browser
