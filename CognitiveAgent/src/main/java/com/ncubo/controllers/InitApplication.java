4bba95392aa7cf3761b4a3f8eec47e1daa2770d3
cguillen <cguillen@testingsoft.com>
2016-11-09 14:25:40 -0600

    Se configura el proyecto para que pueda genrarse un war

--- CognitiveAgent/src/main/java/com/ncubo/controllers/InitApplication.java
+++ CognitiveAgent/src/main/java/com/ncubo/controllers/InitApplication.java
@@ -2,12 +2,21 @@ package com.ncubo.controllers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages = {"com.ncubo"})
-public class InitApplication
+public class InitApplication extends SpringBootServletInitializer 
 {
+
 	public static void main(String[] args) throws Exception 
 	{
 		SpringApplication.run(InitApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+	{
+		return application.sources(InitApplication.class);
 	}
 }
