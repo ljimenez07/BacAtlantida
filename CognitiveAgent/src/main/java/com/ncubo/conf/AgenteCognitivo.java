9b5e5ddbdc117e3d0f3f0a9ce6667cf715db810b
Lisseth <lijimenez@cecropiasolutions.com>
2016-11-17 09:05:16 -0600

    cambios para la tasa de cambio

--- CognitiveAgent/src/main/java/com/ncubo/conf/AgenteCognitivo.java
+++ CognitiveAgent/src/main/java/com/ncubo/conf/AgenteCognitivo.java
@@ -20,6 +20,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import com.ibm.watson.developer_cloud.conversation.v1.ConversationService;
+import com.ibm.watson.developer_cloud.conversation.v1.model.Entity;
 import com.ibm.watson.developer_cloud.conversation.v1.model.Intent;
 import com.ibm.watson.developer_cloud.conversation.v1.model.MessageRequest;
 import com.ibm.watson.developer_cloud.conversation.v1.model.MessageResponse;
@@ -89,7 +90,7 @@ public class AgenteCognitivo
 		
 		System.out.println("contexto de watson cuando sale "+ usuario.getContextoDeWatson());
 		String intent = getIntent(response);
-
+		String texto = getText(response);
 		if(intent.equals(Intencion.SALDO.toString()) && usuario.estaLogueado())
 		{
 
@@ -102,12 +103,12 @@ public class AgenteCognitivo
 			List<?> lista = cuentaColeccion.get("cuentaItem");
 			NodeImpl saldoColecion = (NodeImpl) lista.get(0);
 			NodeImpl saldoContable = saldoColecion.get("saldoColeccion");
-			String texto = getText(response);
 			respuesta.put("texto", texto + saldoContable.get("contable"));
 
 		}
 	
-		else if(intent.equals(Intencion.TASA_DE_CAMBIO.toString()) && usuario.estaLogueado() ){
+		else if(intent.equals(Intencion.TASA_DE_CAMBIO.toString())){
+			
 			String requestBody = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:tas=\"http://hn.infatlan.och/ws/ACD082/out/TasaCambio\"> <soapenv:Header/> <soapenv:Body> <tas:MT_TasaCambio> <activarMultipleEntrada>?</activarMultipleEntrada> <activarParametroAdicional>?</activarParametroAdicional> <!--Optional:--> <transaccionId>100054</transaccionId> <!--Optional:--> <aplicacionId>?</aplicacionId> <paisId>?</paisId> <empresaId>?</empresaId> <!--Optional:--> <regionId>?</regionId> <!--Optional:--> <canalId>?</canalId> <!--Optional:--> <version>?</version> <!--Optional:--> <llaveSesion>?</llaveSesion> <!--Optional:--> <usuarioId>?</usuarioId> <!--Optional:--> <token>?</token> <!--Zero or more repetitions:--> <identificadorColeccion> <!--Optional:--> <was>?</was> <!--Optional:--> <pi>?</pi> <!--Optional:--> <omniCanal>?</omniCanal> <!--Optional:--> <recibo>?</recibo> <!--Optional:--> <numeroTransaccion>?</numeroTransaccion> </identificadorColeccion> <!--Optional:--> <parametroAdicionalColeccion> <!--Zero or more repetitions:--> <parametroAdicionalItem> <linea>?</linea> <!--Optional:--> <tipoRegistro>?</tipoRegistro> <!--Optional:--> <valor>?</valor> </parametroAdicionalItem> </parametroAdicionalColeccion> <!--Optional:--> <logColeccion> <!--Zero or more repetitions:--> <logItem> <identificadorWas>?</identificadorWas> <!--Optional:--> <identificadorPi>?</identificadorPi> <!--Optional:--> <identificadorOmniCanal>?</identificadorOmniCanal> <!--Optional:--> <identificadorRecibo>?</identificadorRecibo> <!--Optional:--> <numeroPeticion>?</numeroPeticion> <!--Optional:--> <identificadorNumeroTransaccion>?</identificadorNumeroTransaccion> <!--Optional:--> <aplicacionId>?</aplicacionId> <!--Optional:--> <canalId>?</canalId> <!--Optional:--> <ambienteId>?</ambienteId> <!--Optional:--> <transaccionId>?</transaccionId> <!--Optional:--> <accion>?</accion> <!--Optional:--> <tipo>?</tipo> <!--Optional:--> <fecha>?</fecha> <!--Optional:--> <hora>?</hora> <!--Optional:--> <auxiliar1>?</auxiliar1> <!--Optional:--> <auxiliar2>?</auxiliar2> <!--Optional:--> <parametroAdicionalColeccion> <!--Zero or more repetitions:--> <parametroAdicionalItem> <linea>?</linea> <!--Optional:--> <tipoRegistro>?</tipoRegistro> <!--Optional:--> <valor>?</valor> </parametroAdicionalItem> </parametroAdicionalColeccion> </logItem> </logColeccion> </tas:MT_TasaCambio> </soapenv:Body> </soapenv:Envelope>";
 
 			String responseXML = given().body(requestBody).post(wsTasaCambio).andReturn().asString();
@@ -116,19 +117,26 @@ public class AgenteCognitivo
 			NodeChildrenImpl body = xmlPath.get("Body");
 			NodeImpl tasa = body.get(0).get("MT_TasaCambioResponse");
 			List<?> codigo = tasa.getNode("Respuesta").getNode("tasaCambioColeccion").get("tasaCambioItem");
-			NodeImpl nodeTipoCambio1 = (NodeImpl) codigo.get(0);
-			NodeImpl moneda = nodeTipoCambio1.get("moneda");
-			NodeImpl compra = nodeTipoCambio1.get("compra");
-			NodeImpl venta = nodeTipoCambio1.get("venta");
-			String tipoCambio1 = moneda +": " +  compra + " " + venta;
-			NodeImpl nodeTipoCambio2 = (NodeImpl) codigo.get(1);
-			moneda = nodeTipoCambio2.get("moneda");
-			compra = nodeTipoCambio2.get("compra");
-			venta = nodeTipoCambio2.get("venta");
-			String tipoCambio2 = moneda +": " +  compra + " " + venta;
 			
-			String texto = getText(response);
-			respuesta.put("texto", texto + tipoCambio1 + " " + tipoCambio2);
+			for(int i = 0; i < codigo.size(); i++)
+			{
+				NodeImpl nodeTipoCambio1 = (NodeImpl) codigo.get(i);
+				NodeImpl moneda = nodeTipoCambio1.get("moneda");
+				NodeImpl compra = nodeTipoCambio1.get("compra");
+				NodeImpl venta = nodeTipoCambio1.get("venta");
+				if(moneda.getValue().equals(Entidad.DOLAR)){
+					
+			
+				texto = texto.replaceAll("%dc", compra.toString());
+				texto = texto.replaceAll("%dv", venta.toString());
+				}
+				if(moneda.getValue().equals(Entidad.EURO)){
+
+					texto = texto.replaceAll("%ec", compra.toString());
+					texto = texto.replaceAll("%ev", venta.toString());			
+				}
+			}
+			respuesta.put("texto", texto);
 		}
 		else if(intent.equals(Intencion.MOVIMIENTOS.toString()) && usuario.estaLogueado())
 		{
@@ -159,13 +167,11 @@ public class AgenteCognitivo
 				
 				last--;
 			}
-			String texto = getText(response);
 			respuesta.put("texto", texto + movimientos );
 			
 		}
 		else
 		{
-			String texto = getText(response);
 			respuesta.put("texto", texto);
 		}
 		return respuesta.toString();
@@ -213,6 +219,16 @@ public class AgenteCognitivo
 		return stringBuilder.toString();
 	}
 	
+	public String[] getEntities(MessageResponse response)
+	{
+		List<Entity> entities = response.getEntities();
+		String[] entitiesArray = new String [entities.size()];
+		for(int i = 0; i < entities.size(); i++ )
+		{
+			entitiesArray[i]= entities.get(i).getEntity();
+		}
+		return entitiesArray;
+	}
 	
 	public String getWsTasaCambio() {
 		return wsTasaCambio;
