cb1ce84e064907f7afc91ed069ce7618775cafb8
vcenteno <vcenteno@cecropiasolutions.com>
2016-11-21 15:49:02 -0600

    Se añadieron recomendaciones de la pantalla de gráficos.

--- CognitiveAgent/src/main/webapp/js/Graficos.js
+++ CognitiveAgent/src/main/webapp/js/Graficos.js
@@ -1,91 +1,127 @@
 /**
- * Dont repeat yourself... cambiar para usar solo una función de graficos.
+ * Graficos para obtener una visualización de cuantos me gusta y no me gusta se han dado a las promociones.
  */
 
-        
-function graficarMegusta(seriesMegusta, labelsMegusta)
+var graficoMeGusta = null;  
+var graficoNoMeGusta = null;
+var h3MeGusta = null;
+var h3NoMeGusta = null;
+
+function graficoDePastel(labelsMegusta, seriesMegusta, labelsNomegusta, seriesNomegusta)
 {
-    var data = 
-    {
-        labels: labelsMegusta,
-        series: 
-        [
-        	seriesMegusta
-        ]
-    };
+	if(graficoMeGusta != null && graficoNoMeGusta != null)
+	{
+		graficoMeGusta.destroy();
+		graficoNoMeGusta.destroy();
+	}
 
-    var options = { seriesBarDistance: 15 };
-
-    var responsiveOptions = [
-      ['screen and (min-width: 641px) and (max-width: 1024px)', {
-        seriesBarDistance: 5,
-        axisX: {
-          labelInterpolationFnc: function (value) {
-            return value;
-          }
-        }
-      }],
-      ['screen and (max-width: 640px)', {
-        seriesBarDistance: 10,
-        axisX: {
-          labelInterpolationFnc: function (value) {
-            return value[0];
-          }
-        }
-      }]
-    ];
-
-    new Chartist.Bar('#grafico-megusta', data, options, responsiveOptions);
+	crearTitulosParaLosGraficos();
+	
+	var ctx = document.getElementById('megusta');
+	var ctx2 = document.getElementById('nomegusta');
+	
+	var colores = [];
+	for(etiqueta of labelsMegusta)
+	{
+		colores.push(generarColor());
+	}
+	generarColor();
+	
+	var data = 
+		{
+		    labels: labelsMegusta,
+		    datasets: 
+		    	[{
+		            data: seriesMegusta,
+		            backgroundColor: colores,
+		            hoverBackgroundColor:colores
+		        }]
+		};
+	var dataNomegusta = 
+	{
+	    labels: labelsNomegusta,
+	    datasets: 
+	    	[{
+	            data: seriesNomegusta,
+	            backgroundColor: colores,
+	            hoverBackgroundColor:colores
+	        }]
+	};
+	
+	// me gusta
+		graficoMeGusta = new Chart(ctx,{
+	    type: 'pie',
+	    data: data,
+	    options: {
+	        scales: {
+	            yAxes: [{
+	                display:false
+	            }]
+	        }
+	    }
+	});
+		
+	// no me gusta
+		graficoNoMeGusta = new Chart(ctx2,{
+		    type: 'pie',
+		    data: dataNomegusta,
+		    options: {
+		        scales: {
+		            yAxes: [{
+		                display:false
+		            }]
+		        }
+		    }
+		});	
+	
 }
 
-function graficarNoMegusta(seriesNoMegusta, labelsNoMegusta)
+function crearTitulosParaLosGraficos()
 {
-    var data = 
-    {
-        labels: labelsNoMegusta,
-        series: 
-        [
-                seriesNoMegusta
-        ]
-    };
-
-    var options = { seriesBarDistance: 15 };
-
-    var responsiveOptions = [
-      ['screen and (min-width: 641px) and (max-width: 1024px)', {
-        seriesBarDistance: 5,
-        axisX: {
-          labelInterpolationFnc: function (value) {
-            return value;
-          }
-        }
-      }],
-      ['screen and (max-width: 640px)', {
-        seriesBarDistance: 10,
-        axisX: {
-          labelInterpolationFnc: function (value) {
-            return value[0];
-          }
-        }
-      }]
-    ]; 
-    
-    new Chartist.Bar('#grafico-no-megusta', data, options, responsiveOptions);
+	var ctx = document.getElementById("titulo-megusta");
+	var ctx2 = document.getElementById("titulo-Nomegusta");
+	
+	if(h3MeGusta != null && h3NoMeGusta != null)
+	{
+		ctx.removeChild(h3MeGusta);
+		ctx2.removeChild(h3NoMeGusta);
+	}
+	var ctx = document.getElementById("titulo-megusta");
+	var ctx2 = document.getElementById("titulo-Nomegusta");
+	h3MeGusta = document.createElement("h3");
+	h3NoMeGusta = document.createElement("h3");
+	
+	var textMeGusta = document.createTextNode("Me gusta");
+	var textNoMeGusta = document.createTextNode("No me gusta");
+	
+	h3MeGusta.appendChild(textMeGusta);
+	h3NoMeGusta.appendChild(textNoMeGusta);	
+	ctx.appendChild(h3MeGusta);
+	ctx2.appendChild(h3NoMeGusta);
 }
 
-function graficar(seriesMegusta, labelsMegusta, seriesNoMegusta, labelsNoMegusta)
+function generarColor()
 {
-    graficarMegusta(seriesMegusta, labelsMegusta);
-    graficarNoMegusta(seriesNoMegusta, labelsNoMegusta);
+	//var color = '#'+Math.floor(Math.random()*16777215).toString(16);
+	var color = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+	return color;
 }
-        
+
 $(function()
     {
         $("#fecha-desde").datepicker({
-        	  dateFormat: "yy-mm-dd"
+        	  dateFormat: "yy-mm-dd",
+        	    beforeShow : function(){
+                    jQuery( this ).datepicker('option','maxDate', jQuery('#fecha-hasta').val() );
+        } 
         });
         $("#fecha-hasta").datepicker({
-        	  dateFormat: "yy-mm-dd"
+        	  dateFormat: "yy-mm-dd",
+        	  beforeShow : function(){
+                  jQuery( this ).datepicker('option','minDate',    jQuery('#fecha-desde').val() );
+        	  } 
         });
+        
+        
     }
  );
