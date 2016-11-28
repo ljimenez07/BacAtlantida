6767f1fd3a38a5b975236ebb3eac2cf3c68ffef5
rdiaz <renandiazreyes@gmail.com>
2016-11-28 11:57:39 -0500

    Se elimina el plugin de status-bar de cordova

--- CognitiveAgent/src/main/webapp/movil/www/js/oferta.handlebars.js
+++ CognitiveAgent/src/main/webapp/movil/www/js/oferta.handlebars.js
@@ -1,36 +1,36 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
 templates['oferta'] = template({"1":function(container,depth0,helpers,partials,data) {
-    return "			<div class=\"html-de-oferta\"></div>\r\n";
+    return "			<div class=\"html-de-oferta\"></div>\n";
 },"3":function(container,depth0,helpers,partials,data) {
     var helper;
 
   return "			<img src=\"/archivossubidos/"
     + container.escapeExpression(((helper = (helper = helpers.imagenPublicidadPath || (depth0 != null ? depth0.imagenPublicidadPath : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"imagenPublicidadPath","hash":{},"data":data}) : helper)))
-    + "\" class=\"imagen-de-oferta\">\r\n";
+    + "\" class=\"imagen-de-oferta\">\n";
 },"5":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
   return "						<div class=\"col-xs-3\"><div class=\"boton-like "
     + alias4(((helper = (helper = helpers.like || (depth0 != null ? depth0.like : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"like","hash":{},"data":data}) : helper)))
-    + "\"></div></div>\r\n						<div class=\"col-xs-3\"><div class=\"boton-dislike "
+    + "\"></div></div>\n						<div class=\"col-xs-3\"><div class=\"boton-dislike "
     + alias4(((helper = (helper = helpers.dislike || (depth0 != null ? depth0.dislike : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"dislike","hash":{},"data":data}) : helper)))
-    + "\"></div></div>\r\n";
+    + "\"></div></div>\n";
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1, helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "<div class=\"row\">\r\n	<div class=\"col-xs-12\">\r\n"
+  return "<div class=\"row\">\n	<div class=\"col-xs-12\">\n"
     + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.esHtml : depth0),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.program(3, data, 0),"data":data})) != null ? stack1 : "")
-    + "		<div class=\"row nombre-del-comercio\">\r\n			<div class=\"col-xs-12\">\r\n				<strong>"
+    + "		<div class=\"row nombre-del-comercio\">\n			<div class=\"col-xs-12\">\n				<strong>"
     + alias4(((helper = (helper = helpers.comercio || (depth0 != null ? depth0.comercio : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"comercio","hash":{},"data":data}) : helper)))
-    + "</strong>\r\n			</div>\r\n		</div>\r\n		<div class=\"row\">\r\n			<div class=\"col-xs-12\">\r\n				<span>"
+    + "</strong>\n			</div>\n		</div>\n		<div class=\"row\">\n			<div class=\"col-xs-12\">\n				<span>"
     + alias4(((helper = (helper = helpers.tituloDeOferta || (depth0 != null ? depth0.tituloDeOferta : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"tituloDeOferta","hash":{},"data":data}) : helper)))
-    + "</span>\r\n			</div>\r\n		</div>\r\n		<div class=\"row contenedor-de-botones\">\r\n			<div class=\"col-xs-12\">\r\n				<div class=\"tiempo-transcurrido\">Hace "
+    + "</span>\n			</div>\n		</div>\n		<div class=\"row contenedor-de-botones\">\n			<div class=\"col-xs-12\">\n				<div class=\"tiempo-transcurrido\">Hace "
     + alias4(((helper = (helper = helpers.tiempoTranscurrido || (depth0 != null ? depth0.tiempoTranscurrido : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"tiempoTranscurrido","hash":{},"data":data}) : helper)))
-    + "</div>\r\n			</div>\r\n		</div>\r\n		<div class=\"row\">\r\n			<div class=\"col-xs-12 text-center boton-escuchar-oferta\" data-audio=\""
+    + "</div>\n			</div>\n		</div>\n		<div class=\"row\">\n			<div class=\"col-xs-12 text-center boton-escuchar-oferta\" data-audio=\""
     + alias4(((helper = (helper = helpers.descripcionAudio || (depth0 != null ? depth0.descripcionAudio : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"descripcionAudio","hash":{},"data":data}) : helper)))
-    + "\">\r\n				<img src=\"./img/icono-94x91.png\">\r\n			</div>\r\n		</div>\r\n		<div class=\"row contenedor-de-botones\">\r\n			<div class=\"col-xs-6\">\r\n				<div class=\"row\">\r\n"
+    + "\">\n				<img src=\"./img/icono-94x91.png\">\n			</div>\n		</div>\n		<div class=\"row contenedor-de-botones\">\n			<div class=\"col-xs-6\">\n				<div class=\"row\">\n"
     + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.esUnUsuarioConocido : depth0),{"name":"if","hash":{},"fn":container.program(5, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + "				</div>\r\n			</div>\r\n			<div class=\"col-xs-6\">\r\n				<div class=\"row\">\r\n					<div class=\"col-xs-9\"></div>\r\n						<div class=\"col-xs-3\"><div class=\"boton-compartir\"></div></div>\r\n				</div>\r\n			</div>\r\n		</div>\r\n	</div>\r\n</div>";
+    + "				</div>\n			</div>\n			<div class=\"col-xs-6\">\n				<div class=\"row\">\n					<div class=\"col-xs-9\"></div>\n						<div class=\"col-xs-3\"><div class=\"boton-compartir\"></div></div>\n				</div>\n			</div>\n		</div>\n	</div>\n</div>";
 },"useData":true});
 })();
