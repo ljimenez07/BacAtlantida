73f69e4feee545a41745720a83c932761b15ab7b
cguillen <cguillen@testingsoft.com>
2016-12-02 13:12:16 -0600

    clase que sergio no subio

---
+++ LogicaDeConversaciones/src/main/java/com/ncubo/chatbot/contexto/ValoresDeLasVariablesDeContexto.java
@@ -0,0 +1,12 @@
+package com.ncubo.chatbot.contexto;
+
+import java.util.HashMap;
+
+public class ValoresDeLasVariablesDeContexto {
+
+	private HashMap<String, String> misValoresDeContexto = new HashMap<>();
+	
+	public ValoresDeLasVariablesDeContexto(){}
+	
+	public void agregarValor(String )
+}
