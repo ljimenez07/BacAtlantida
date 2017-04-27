62b0b4717464c1acbaf998078e640f5c562c21f7
SergioGQ <sgonzales@cecropiasolutions.com>
2017-04-27 10:18:54 -0600

    Se actualizaron los properties

--- CognitiveAgent/src/main/webapp/movil/run.bat
+++ CognitiveAgent/src/main/webapp/movil/run.bat
@@ -1,10 +1,12 @@
 @echo off
 
 @echo var serverDomain = "%1"; > ./www/js/globals.js
-call handlebars ./www/plantillas/chats.handlebars -f ./www/js/chats.handlebars.js 
-call handlebars ./www/plantillas/layout.handlebars -f ./www/js/layout.handlebars.js 
-call handlebars ./www/plantillas/ofertas.handlebars -f ./www/js/ofertas.handlebars.js 
-call handlebars ./www/plantillas/listaDeOfertas.handlebars -f ./www/js/listaDeOfertas.handlebars.js 
-call handlebars ./www/plantillas/oferta.handlebars -f ./www/js/oferta.handlebars.js 
-call handlebars ./www/plantillas/login.handlebars -f ./www/js/login.handlebars.js 
+call handlebars ./www/plantillas/chats.handlebars -f ./www/js/chats.handlebars.js
+call handlebars ./www/plantillas/layout.handlebars -f ./www/js/layout.handlebars.js
+call handlebars ./www/plantillas/ofertas.handlebars -f ./www/js/ofertas.handlebars.js
+call handlebars ./www/plantillas/listaDeOfertas.handlebars -f ./www/js/listaDeOfertas.handlebars.js
+call handlebars ./www/plantillas/oferta.handlebars -f ./www/js/oferta.handlebars.js
+call handlebars ./www/plantillas/popupconocerte.handlebars -f ./www/js/popupconocerte.handlebars.js
+call handlebars ./www/plantillas/login.handlebars -f ./www/js/login.handlebars.js
+
 cordova run android
